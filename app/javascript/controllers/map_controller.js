89cfaeb63a5c9e05f8075424d81e9933e35c0f9d
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-01-19 23:21:24 -0500

    added stimulus static values

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,12 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
+
+  static values = {
+    apiKey: String
+  }
+
   connect() {
-    mapboxgl.accessToken = ENV['MAPBOX_API_KEY'];
+    mapboxgl.accessToken = this.apiKeyValue;
     this.map = new mapboxgl.Map({
       container: this.element, // container ID (puede ser map ahhi tmb ya que es el id del container)
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      console.log(this.element)
     });
   }
 }
