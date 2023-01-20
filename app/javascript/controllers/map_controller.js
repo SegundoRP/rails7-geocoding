8ce6ea5879ef8776d55bed3a8f1863bccdc84526
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-01-19 23:14:24 -0500

    update stimulus controller

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -3,6 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="map"
 export default class extends Controller {
   connect() {
-    console.log('Hello')
+    mapboxgl.accessToken = ENV['MAPBOX_API_KEY'];
+    this.map = new mapboxgl.Map({
+      container: this.element, // container ID (puede ser map ahhi tmb ya que es el id del container)
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      console.log(this.element)
+    });
   }
 }
