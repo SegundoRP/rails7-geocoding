c8554364a92e326b8757aa699d0d74ee4d3a40fd
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-01-23 22:59:22 -0500

    added fit markers to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -15,14 +15,23 @@ export default class extends Controller {
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
     });
 
-    this.#addMarkerstoMap();
+    this.#addMarkersToMap();
+    this.#fitMarkersToMap();
   }
 
-  #addMarkerstoMap() {
+  // agrega marcadores de tus coordenadas en el mapa
+  #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
         .addTo(this.map);
     });
+  };
+
+  // te posiciona la vista en el mapa (mas cerca)
+  #fitMarkersToMap() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   };
 }
